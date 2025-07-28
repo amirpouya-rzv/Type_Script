54fc768c13a323dbe1bf6d00ac031ea4d91b5483
amirpouya-rzv <amir.rezvani7997@gmail.com>
2025-07-29 00:41:13 +0330

    header completed.

--- src/Layout/header/Header.tsx
+++ src/Layout/header/Header.tsx
@@ -3,6 +3,9 @@ import { useDispatch } from 'react-redux';
 import { setShowSidebar } from '../../redux/ui_management/uiManagement';
 import { CgMenuHotdog } from 'react-icons/cg';
 import { converMiladi2Jalali } from '@/utils/dateutils';
+import { GoBell } from 'react-icons/go';
+import { IoPersonOutline } from 'react-icons/io5';
+import { RxDot } from 'react-icons/rx';
 
 const Header = () => {
 
@@ -11,12 +14,19 @@ const Header = () => {
     return (
         <section id="header" className="fixed top-0 h-app_header_h w-full shadow-lg bg-slate-200 
  dark:bg-[#25232c] dark:text-white md:pr-app_sidebar_w border-b border-rose-600 dark:shadow-rose-800 border-b-rose-600 dark:border-rose-600">
-            <div className='items-center p-4'>
+            <div className='flex justify-between p-4'>
                 <button className='md:hidden mt-4 mx-2' onClick={() => dispatch(setShowSidebar(true))}>
                     <CgMenuHotdog size={24} />
                 </button>
-                <p className='hidden md:block'>{converMiladi2Jalali()}</p>
-                
+                <p className='h idden md:block'>{converMiladi2Jalali(undefined, "ddd. jD jMMMM jYYYY")}</p>
+                <span className='flex gap-2 justify-end items-center '>
+                    <button><GoBell size={24} className='text-yellow-500'/>
+                        <RxDot className='absolute top-4 text-rose-600 animate-ping' />
+                    </button>
+                    <button className='border-2 text-sky-500 rounded-full p-1 border-gray-300'><IoPersonOutline size={24} />
+                    </button>
+                    <p>نام کاربر</p>
+                </span>
             </div>
 
         </section>
